21f83dce0fc20572cb1fb0d0194e78d6141a5a4e
Jiwannn <Jiwann1017@gmail.com>
2025-11-22 15:31:04 +0800

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,13 @@
-import "./globals.css";
+import "../globals.css";
 
 export const metadata = {
-  title: "Dark Mode To-Do / Grocery List",
-  description: "Modern dark-themed grocery and task list app",
+  title: "To-Do List",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-900 text-gray-100">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
